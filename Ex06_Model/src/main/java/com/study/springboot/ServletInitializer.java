7a32e617998dc6221c2e209f5603ea3047c85bf1
manic-063g <manic-063g@manic-063>
2022-01-13 17:18:09 +0900

    여기를 클릭

--- Ex06_Model/src/main/java/com/study/springboot/ServletInitializer.java
+++ Ex06_Model/src/main/java/com/study/springboot/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("여기 클릭");
 		return application.sources(Ex06ModelApplication.class);
 	}
 
